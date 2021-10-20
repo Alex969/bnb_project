a4c76587b2ce5b2e949468caf6fef25ca7c3ad5a
Bernard Leanse <bernardleanse96@gmail.com>
2021-10-20 11:10:51 +0100

    completed create listing

--- lib/listing.rb
+++ lib/listing.rb
@@ -14,9 +14,12 @@ class Listing
   def self.create(title:, description:, price_per_night:, avail_from:, avail_to:, user_id:)
     query = DatabaseConnection.query("INSERT INTO listings (title, description, price_per_night)
     VALUES($1, $2, $3) RETURNING id, title, description, price_per_night, user_id;",[title, description, price_per_night]).first
-    Listing.new(id: query['id'], title: query['title'], description: query['description'], price_per_night: query['price_per_night'],user_id: query['user_id'])
-    # booking_query = DatabaseConnection.query("INSERT INTO bookings (")
-     # availto - availfrom .times create Booking instance until index reaches limit - availfrom +1 being the date.
+    listing = Listing.new(id: query['id'], title: query['title'], description: query['description'], price_per_night: query['price_per_night'],user_id: query['user_id'])
+    
+    date_range(avail_from, avail_to).each do |date|
+      DatabaseConnection.query("INSERT INTO bookings(listing_id, date) VALUES ($1, $2)", [listing.id, date])
+    end
+
   end
 
  
@@ -25,5 +28,15 @@ class Listing
     result.map do |listing|
       Listing.new(id: listing['id'], title: listing['title'], description: listing['description'], price_per_night: listing['price_per_night'],user_id: listing['user_id'])
     end
+  end
+
+  
+
+  private
+
+  def self.date_range(avail_from, avail_to)
+    start_date_object = Date.parse(avail_from)
+    end_date_object = Date.parse(avail_to)
+    range = (start_date_object..end_date_object).map(&:to_s)
   end
 end
