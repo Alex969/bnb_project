be31b37f6ee0403e1ba26d48610a41ee456d3520
Kostas Ntanos <kostas@ntanos.co.uk>
2021-10-21 13:04:11 +0100

    Done pending requests

--- lib/request.rb
+++ lib/request.rb
@@ -1,12 +1,29 @@
 class Request
+ 
+  attr_reader :id, :booking_id, :user_id
 
-  def initialize(booking_id:, user_id:)
-
+  def initialize(id:, booking_id:, user_id:)
+    @id = id
+    @booking_id = booking_id
+    @user_id = user_id
   end
 
 
   def self.create(booking_id:, user_id:)
-    Request.new(booking_id: booking_id, user_id: user_id)
+    query = DatabaseConnection.query("INSERT INTO requests (booking_id, user_id) VALUES($1, $2) RETURNING id, booking_id, user_id;", [booking_id, user_id]).first
+    Request.new(id: query['id'], booking_id: query['booking_id'], user_id: query['user_id'])
   end
 
+  def self.all(user_id:)
+    query = DatabaseConnection.query("SELECT listings.title, bookings.date 
+      FROM requests
+      INNER JOIN bookings 
+      ON requests.booking_id = bookings.id
+      INNER JOIN listings
+      ON bookings.listing_id = listings.id
+      WHERE requests.user_id = $1;", [user_id])
+    query.map do | pending_booking |
+      pending_booking
+    end
+  end     
 end
--- spec/features/requesting_booking_spec.rb
+++ spec/features/requesting_booking_spec.rb
@@ -9,4 +9,17 @@ feature 'Requesting booking' do
     click_button('Request Booking', match: :first)
     expect(page).to have_content 'Your booking has been requested'
   end
+
+  scenario 'clicked listing shows on view all requests page under requests made (pending)' do
+    sign_up
+    log_in
+    create_test_listing
+
+    visit '/listings'
+    click_link 'Test Listing'
+    click_button('Request Booking', match: :first)
+
+    visit '/requests'
+    expect(page).to have_content 'Test Listing'
+  end
 end
