94db1cc0ab505c927227cc7aee6574b24ba6bb40
AJOsmaston <dajoxx@gmail.com>
2021-10-21 16:22:07 +0100

    added email check and flash notice

--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -8,4 +8,16 @@ feature 'Register' do
 
     expect(page).to have_content 'Please sign in to continue'
   end
+
+  scenario 'prevents users registering with the same email' do
+    sign_up
+    visit('/')
+    fill_in 'email', with: 'test@test.com'
+    fill_in 'username', with: 'Mr. Test'
+    fill_in 'password', with: 'password'
+    click_button 'Submit'
+
+    expect(page).to have_content "Error: Email already exists"
+
+  end
 end
